9c80451c468834f045b7839523296198a9bce447
Ilton Silveira <iltonsilveira91@gmail.com>
2021-12-03 08:54:29 -0300

    preventing sign_up if app not allow it

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,49 +1,51 @@
 # frozen_string_literal: true
 
-class Users::RegistrationsController < Devise::RegistrationsController
-  layout 'sign'
-  before_action :setup_dependencies
+module Users
+  class RegistrationsController < Devise::RegistrationsController
+    layout 'sign'
+    before_action :setup_dependencies
 
-  before_action :configure_sign_up_params, only: [:create]
-  before_action :configure_account_update_params, only: [:update]
-  after_action :setup_pending_mail, only: [:create]
+    before_action :configure_sign_up_params, only: [:create]
+    before_action :configure_account_update_params, only: [:update]
+    after_action :setup_pending_mail, only: [:create]
 
-  # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+    # GET /resource/sign_up
+    # def new
+    #   super
+    # end
 
-  # POST /resource
-  def create
-    flash[:info] = 'Lembre-se de confirmar seu email'
-    super
-  end
+    # POST /resource
+    def create
+      flash[:info] = 'Lembre-se de confirmar seu email'
+      super
+    end
 
-  # GET /resource/edit
-  def edit
-    super
-  end
+    # GET /resource/edit
+    # def edit
+    #   super
+    # end
 
-  # PUT /resource
-  def update
-    super
-  end
+    # PUT /resource
+    # def update
+    #   super
+    # end
 
-  # DELETE /resource
-  # def destroy
-  #   super
-  # end
+    # DELETE /resource
+    # def destroy
+    #   super
+    # end
+  
+    # GET /resource/cancel
+    # Forces the session data which is usually expired after sign
+    # in to be expired now. This is useful if the user wants to
+    # cancel oauth signing in/up in the middle of the process,
+    # removing all OAuth session data.
+    # def cancel
+    #   super
+    # end
 
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
-
-  protected
+    protected
+    
     # Setup Cookies to show up the email pending page
     def setup_pending_mail
       # TODO: refactor if using parallelism on server REMOVE/REFACTOR IT
@@ -60,17 +62,19 @@ class Users::RegistrationsController < Devise::RegistrationsController
       devise_parameter_sanitizer.permit(:account_update, keys: %i[locale full_name email email_confirmation password password_confirmation company_fantasy_name company_legal_name website description timezone gov_id_pf remember_me])
     end
 
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+    # The path used after sign up.
+    # def after_sign_up_path_for(resource)
+    #   super(resource)
+    # end
 
-  # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+    # The path used after sign up for inactive accounts.
+    # def after_inactive_sign_up_path_for(resource)
+    #   super(resource)
+    # end
 
-  private
+    private
     def setup_dependencies
+      return redirect_to '/' unless @app&.registrable?
     end
+  end
 end
