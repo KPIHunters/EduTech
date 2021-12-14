1536f15c7239d68080dfeb929278d88ad884c786
Ilton Silveira <iltonsilveira91@gmail.com>
2021-12-14 01:12:25 -0300

    required fields highlighted

--- db/migrate/20210215182839_add_birthday_to_profile.rb
+++ db/migrate/20210215182839_add_birthday_to_profile.rb
@@ -1,5 +1,5 @@
 class AddBirthdayToProfile < ActiveRecord::Migration[6.0]
   def change
-    add_column :profiles, :birthday, :date
+    add_column :profiles, :birthday, :date, null: false
   end
 end
